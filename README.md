a757b69dcb060d858bcee9c9766d2196413f2e37
gupagnosi <guto.dev20@gmail.com>
2024-09-11 10:50:33 -0300

    finished tests and add postman colection

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Faça a clone do projeto.
-2. Instale as dependências utilizando o comando "composer install", lembrando que é necessário ter instalado o PHP e o composer no seu computador.
+2. Instale as dependências utilizando o comando "composer install", lembrando que é necessário ter instalado o PHP 8.2, mysql 8.0 e o composer no seu computador.
 3. Certifique de configurar o ENV de acordo com sua necessidade.
 4. Executa o comando "php artisan migrate" para criar as tabelas, caso ja tenha executado e precise resetar utilize o comando "php artisan migrate:refresh";
 5. Executa o comando "php artisan db:seed" para colocar algumas informações já na sua tabela de produtos.
--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -10,10 +10,151 @@ class ProductTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_a_basic_request(): void
+    public function test_returns_all_products()
     {
-        $response = $this->get('/');
+        Product::factory()->count(3)->create();
+
+        $response = $this->get('/api/v1/products');
 
         $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            '*' => [
+                'id',
+                'name',
+                'price',
+                'status',
+                'stock_quantity',
+                'description',
+                'created_at',
+                'updated_at'
+            ]
+        ]);
+    }
+
+    public function test_returns_a_product_by_id()
+    {
+        $product = Product::factory()->create();
+
+        $response = $this->get('/api/v1/products/' . $product->id);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'id' => $product->id,
+            'name' => $product->name,
+            'price' => $product->price,
+            'status' => $product->status,
+            'stock_quantity' => $product->stock_quantity,
+            'description' => $product->description,
+            'created_at' => $product->created_at,
+            'updated_at' => $product->updated_at,
+        ]);
+    }
+
+    /** @test */
+    public function test_returns_404_when_product_not_found()
+    {
+        $response = $this->get('/api/v1/products/9999999');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_creates_a_product()
+    {
+
+        $data = [
+            'name' => 'Test Product',
+            'price' => 100,
+            'status' => 'em estoque',
+            'description' => 'This is a test product',
+            'stock_quantity' => 50
+        ];
+
+        $response = $this->post('/api/v1/products', $data);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('products', $data);
+    }
+
+    /** @test */
+    public function test_fails_to_create_product_with_invalid_data()
+    {
+
+        $data = [
+            'name' => '',
+            'price' => 'invalid',
+        ];
+
+        $response = $this->post('/api/v1/products', $data);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['name', 'price']);
+    }
+
+    public function test_updates_a_product()
+    {
+        $product = Product::factory()->create();
+
+        $data = [
+            'name' => 'Updated Product',
+            'price' => 150,
+            'status' => 'em reposição',
+            'description' => 'Updated description',
+            'stock_quantity' => 60
+        ];
+
+        $response = $this->put('/api/v1/products/' . $product->id, $data);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('products', $data);
+    }
+
+    /** @test */
+    public function test_fails_to_update_product_with_invalid_data()
+    {
+
+        $product = Product::factory()->create();
+
+        $data = [
+            'name' => '',
+            'price' => 'invalid',
+        ];
+
+        $response = $this->put('/api/v1/products/' . $product->id, $data);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['name', 'price']);
+    }
+
+    /** @test */
+    public function test_returns_404_when_updating_non_existent_product()
+    {
+        $data = [
+            'name' => 'Updated Product',
+            'price' => 150,
+        ];
+
+        $response = $this->put('/api/v1/products/9999999', $data);
+
+        $response->assertStatus(404);
+    }
+    public function test_deletes_a_product()
+    {
+        $product = Product::factory()->create();
+
+        $response = $this->delete('/api/v1/products/' . $product->id);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('products', ['id' => $product->id]);
+    }
+
+    /** @test */
+    public function test_returns_404_when_deleting_non_existent_product()
+    {
+        $response = $this->delete('/api/v1/products/9999999');
+        $response->assertStatus(404);
     }
 }
